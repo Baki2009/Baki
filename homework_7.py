6804bcc9dcb616464256206ae8e4a9d12491069c
Baki2009 <bakajsamatov095@gmail.com>
2023-10-24 17:40:52 +0600

    commit

--- homework_7.py
+++ homework_7.py
@@ -17,7 +17,7 @@ def create_database_and_table():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            product_title TEXT NOT NULL CHECK(length(product_title) <= 200),
+            product_title VARCHAR (200) NOT NULL,
             price REAL NOT NULL DEFAULT 0.0,
             quantity INTEGER NOT NULL DEFAULT 0
         );"""
