25da90d95db6bd99b19e0ec2e82dd2a88443b2e6
Baki2009 <bakajsamatov095@gmail.com>
2023-10-24 17:18:36 +0600

    commit

--- homework_7.py
+++ homework_7.py
@@ -7,5 +7,41 @@
 # 6. Добавить поле quantity (количество продукта) целочисленного (int) типа данных, поле не должно быть пустым (NOT NULL) значением по-умолчанию поля должно быть 0
 # 7. Добавить функцию которая бы добавляла в БД продукты
 # 8. Добавить функцию которая удаляет продукт по id
+
+import sqlite3
+
+def create_database_and_table():
+    conn = sqlite3.connect('hw.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product_title TEXT NOT NULL CHECK(length(product_title) <= 200),
+            price REAL NOT NULL DEFAULT 0.0,
+            quantity INTEGER NOT NULL DEFAULT 0
+        );"""
+    )
+
+    conn.commit()
+    conn.close()
+
+def add_product(product_title, price, quantity):
+
+    conn = sqlite3.connect('hw.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO products (product_title, price, quantity)
+        VALUES (?, ?, ?)
+    ''', (product_title, price, quantity))
+    conn.commit()
+    conn.close()
+    print("Продукт успешно добавлен в базу данных.")
+
+
+create_database_and_table()
+
+add_product("Новый продукт", 19.99, 10)
+
 # Доп ДЗ:
 # Добавить функцию, которая бы выбирала из БД товары которые дешевле 100 сомов и распечатывала бы их в консоли
